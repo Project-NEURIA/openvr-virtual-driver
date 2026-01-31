a5a6c6698cd637aef0d76a3a95cf07c74e0189d8
xingjianll <4396kevinliu@gmail.com>
2026-01-31 15:19:13 -0500

    Chore: Add release workflow

--- client/src/__init__.py
+++ client/src/__init__.py
@@ -1,4 +1,6 @@
+__version__ = "0.1.0"
+
 from .client import Client, Pose, Frame
 from .vmd import VMDPlayer
 
-__all__ = ["Client", "Pose", "Frame", "VMDPlayer"]
+__all__ = ["Client", "Pose", "Frame", "VMDPlayer", "__version__"]
